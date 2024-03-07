508a44d2f830892bd05691fc61c88658d3e5159c
algifaryadabi <160981125+algifaryadabi@users.noreply.github.com>
2024-03-07 17:55:03 +0700

    Add files via upload

---
+++ MainLibrary.java
@@ -0,0 +1,52 @@
+package File_perpustakaan;
+
+import java.util.Scanner;
+
+public class MainLibrary {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Perpustakaan add = new Perpustakaan();
+
+        while (true) {
+            System.out.printf("=======[ Selamat Datang di Perpustakaan]=======\n"
+
+                    + "Masukkan pilihan anda untuk melihat buku berdasarkan kategori\n"
+                    + "1. Teknologi\n" + "2. Filsafat\n" + "3. Sejarah\n" + "4. Agama\n"
+                    + "5. Psikologi\n" + "6. Politik\n" + "7. Fiksi\n"
+                    + "8. Tampilkan total buku yang tersedia\n" + "9. Keluar dari program\n");
+            System.out.println("Silahkan menginputkan kode yang diinginkan(1-9)");
+
+            int code = scan.nextInt();
+            if (code == 1) {
+                add.showtype(add.Teknologi, "Teknologi");
+            } else if (code == 2) {
+                add.showtype(add.Filsafat, "Filsafat");
+            } else if (code == 3) {
+                add.showtype(add.Sejarah, "Sejarah");
+            } else if (code == 4) {
+                add.showtype(add.Agama, "Agama");
+            } else if (code == 5) {
+                add.showtype(add.Psikologi, "Psikologi");
+            } else if (code == 6) {
+                add.showtype(add.Politik, "Politik");
+            } else if (code == 7) {
+                add.showtype(add.Fiksi, "Fiksi");
+            } else if (code == 8) {
+                System.out.println();
+
+                System.out.println("Terdapat 35 jumlah buku yang ada di Perpustakaan ini");
+
+                System.out.println();
+
+            } else if (code == 9) {
+                break;
+            } else {
+                System.out.println();
+                System.out.println("Angka tidak tersedia, dan buku tidak ada ( Error )");
+                System.out.println();
+            }
+        }
+        scan.close();
+    }
+
+}
