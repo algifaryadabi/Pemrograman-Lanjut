ee83567c3ce2a6995d135dc9cf2927df488a1f3a
algifaryadabi <160981125+algifaryadabi@users.noreply.github.com>
2024-03-14 22:53:30 +0700

    Add files via upload

--- MainLibrary.java
+++ MainLibrary.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class MainLibrary {
     public static void main(String[] args) {
+        Perpustakaan perpus = new Perpustakaan("Judul Default", "Penulis Default", 0, "Sinopsis Default");
         Scanner scan = new Scanner(System.in);
-        Perpustakaan add = new Perpustakaan();
 
         while (true) {
             System.out.printf("=======[ Selamat Datang di Perpustakaan]=======\n"
@@ -18,35 +18,126 @@ public class MainLibrary {
 
             int code = scan.nextInt();
             if (code == 1) {
-                add.showtype(add.Teknologi, "Teknologi");
+                Perpustakaan[] Teknologi = {
+                        new Perpustakaan(
+                                "The Age of Surveillance Capitalism: The Fight for a Human Future at the New Frontier of Power",
+                                "Shoshana Zuboff", 704,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", 464,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan(
+                                "The Innovators: How a Group of Hackers, Geniuses, and Geeks Created the Digital Revolution",
+                                "Walter Isaacson", 560,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan(
+                                "Weapons of Math Destruction: How Big Data Increases Inequality and Threatens Democracy",
+                                "Cathy O'Neil", 272,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("AI Superpowers: China, Silicon Valley, and the New World Order", "Kai-Fu Lee",
+                                272,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?")
+                };
+                perpus.kategori(Teknologi);
             } else if (code == 2) {
-                add.showtype(add.Filsafat, "Filsafat");
+                Perpustakaan[] Filsafat = {
+                        new Perpustakaan("The Philosophy Book: Big Ideas Simply Explained", "DK", 352,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Justice: What's the Right Thing to Do?", "Michael J. Sandel", 320,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("The Big Questions: A Short Introduction to Philosophy",
+                                "Robert C. Solomon, Kathleen M. Higgins", 336,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Thinking, Fast and Slow", "Daniel Kahneman", 512,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("The Consolations of Philosophy", "Alain de Botton", 272,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?")
+                };
+                perpus.kategori(Filsafat);
             } else if (code == 3) {
-                add.showtype(add.Sejarah, "Sejarah");
+                Perpustakaan[] Sejarah = {
+                        new Perpustakaan("A People's History of the United States", "Howard Zinn", 784,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Guns, Germs, and Steel: The Fates of Human Societies", "Jared Diamond", 480,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", 464,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("The Rise and Fall of the Third Reich: A History of Nazi Germany",
+                                "William L. Shirer", 1280,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("The Silk Roads: A New History of the World", "Peter Frankopan", 672,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?")
+                };
+                perpus.kategori(Sejarah);
             } else if (code == 4) {
-                add.showtype(add.Agama, "Agama");
+                Perpustakaan[] Agama = {
+                        new Perpustakaan("The God Delusion", "Richard Dawkins", 464,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Zealot: The Life and Times of Jesus of Nazareth", "Reza Aslan", 336,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan(
+                                "The Case for Christ: A Journalist's Personal Investigation of the Evidence for Jesus",
+                                "Lee Strobel", 336,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Waking Up: A Guide to Spirituality Without Religion", "Sam Harris", 256,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Why Religion?: A Personal Story", "Elaine Pagels", 256,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?")
+                };
+                perpus.kategori(Agama);
             } else if (code == 5) {
-                add.showtype(add.Psikologi, "Psikologi");
+                Perpustakaan[] Psikologi = {
+                        new Perpustakaan("Thinking, Fast and Slow", "Daniel Kahneman", 512,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("The Power of Habit: Why We Do What We Do in Life and Business",
+                                "Charles Duhigg", 400,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Quiet: The Power of Introverts in a World That Can't Stop Talking",
+                                "Susan Cain", 352,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Blink: The Power of Thinking Without Thinking", "Malcolm Gladwell", 296,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Emotional Intelligence: Why It Can Matter More Than IQ", "Daniel Goleman",
+                                384,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?")
+                };
+                perpus.kategori(Psikologi);
             } else if (code == 6) {
-                add.showtype(add.Politik, "Politik");
+                Perpustakaan[] Politik = {
+                        new Perpustakaan("The Righteous Mind: Why Good People are Divided by Politics and Religion",
+                                "Jonathan Haidt", 512,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("The End of History and the Last Man", "Francis Fukuyama", 464,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("The New Jim Crow: Mass Incarceration in the Age of Colorblindness",
+                                "Michelle Alexander", 352,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("How Democracies Die", "Steven Levitsky, Daniel Ziblatt", 320,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("On Tyranny: Twenty Lessons from the Twentieth Century", "Timothy Snyder", 128,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?")
+                };
+                perpus.kategori(Politik);
             } else if (code == 7) {
-                add.showtype(add.Fiksi, "Fiksi");
+                Perpustakaan[] Fiksi = {
+                        new Perpustakaan("The Night Circus", "Erin Morgenstern", 400,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("The Goldfinch", "Donna Tartt", 771,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Circe", "Madeline Miller", 400,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("The Underground Railroad", "Colson Whitehead", 336,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?"),
+                        new Perpustakaan("Normal People", "Sally Rooney", 288,
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus necessitatibus laboriosam similique sunt cupiditate placeat suscipit, quis officiis, itaque quibusdam, rerum fuga natus quas minus velit assumenda perferendis dolorum unde?")
+                };
+                perpus.kategori(Fiksi);
             } else if (code == 8) {
-                System.out.println();
-
-                System.out.println("Terdapat 35 jumlah buku yang ada di Perpustakaan ini");
-
-                System.out.println();
-
-            } else if (code == 9) {
                 break;
             } else {
-                System.out.println();
-                System.out.println("Angka tidak tersedia, dan buku tidak ada ( Error )");
-                System.out.println();
+                System.out.println("Mohon maaf, kode yang Anda masukkan tidak valid.");
             }
         }
+
         scan.close();
     }
-
 }
--- Perpustakaan.java
+++ Perpustakaan.java
@@ -1,101 +1,37 @@
 package File_perpustakaan;
 
 public class Perpustakaan {
-        public String[][] Teknologi = {
-                        { "Clean Architecture: A Craftsman's Guide to Software Structure and Design",
-                                        "Robert C. Martin", "432", " Prentice Hall", "2017" },
-                        { "The Pragmatic Programmer: Your Journey to Mastery", "Andrew Hunt, David Thomas", "352",
-                                        " Addison-Wesley", "1999" },
-                        { "The Innovators: How a Group of Hackers, Geniuses, and Geeks Created the Digital Revolution",
-                                        "Walter Isaacson", "560", "Simon & Schuster", "2014" },
-                        { "Artificial Intelligence: A Modern Approach", "Stuart Russell, Peter Norvig",
-                                        "1152", "Pearson", "2009" },
-                        { "AI Superpowers: China, Silicon Valley, and the New World Order", "Kai-Fu Lee", "272",
-                                        "Houghton Mifflin Harcourt", "2018" }
-        };
+        private String judul;
+        private String penulis;
+        private int jumlahHalaman;
+        private String sinopsis;
 
-        public String[][] Filsafat = {
-                        { "Meditations", "Marcus Aurelius", "Varied (tergantung edisi)", "Various", "2011" },
-                        { "Sophie's World", "Jostein Gaarder", "513", "Farrar, Straus and Giroux",
-                                        "1991" },
-                        { "The Big Questions: A Short Introduction to Philosophy",
-                                        "Robert C. Solomon, Kathleen M. Higgins", "336",
-                                        "Cengage Learning", "2008" },
-                        { "Being and Time", "Martin Heidegger", " Varied (tergantung edisi)", "Various", "1927" },
-                        { "The Consolations of Philosophy", "Alain de Botton", "272", "Penguin Books", "2000" }
-        };
+        public Perpustakaan(String judul, String penulis, int jumlahHalaman, String sinopsis) {
+                this.judul = judul;
+                this.penulis = penulis;
+                this.jumlahHalaman = jumlahHalaman;
+                this.sinopsis = sinopsis;
+        }
 
-        public String[][] Sejarah = {
-                        { "A People's History of the United States", "Howard Zinn", "784",
-                                        "Harper Perennial Modern Classics",
-                                        "1980" },
-                        { "SPQR: A History of Ancient Rome", "Mary Beard", "608 ",
-                                        "Liveright", "2015" },
-                        { "Sapiens: A Brief History of Humankind", "Yuval Noah Harari", "464", "Harper", "2014" },
-                        { "The Rise and Fall of the Third Reich: A History of Nazi Germany", "William L. Shirer",
-                                        "1280 halaman",
-                                        "Simon & Schuster", "1960" },
-                        { "The Silk Roads: A New History of the World", "Peter Frankopan", "672",
-                                        "Bloomsbury Publishing", "2015" }
-        };
+        public int hitungJumlahKataSinopsis() {
+                String[] kataKata = sinopsis.split(" ");
+                return kataKata.length;
+        }
 
-        public String[][] Agama = {
-                        { "The God Delusion", "Richard Dawkins", "464", "Bantam Press", "2006" },
-                        { "Zealot: The Life and Times of Jesus of Nazareth", "Reza Aslan", "336", "Random House",
-                                        "2013" },
-                        { "The Case for Christ: A Journalist's Personal Investigation of the Evidence for Jesus",
-                                        "Lee Strobel",
-                                        "336", "Zondervan", "1998" },
-                        { "Waking Up: A Guide to Spirituality Without Religion", "Sam Harris", "256",
-                                        "Simon & Schuster", "2014" },
-                        { "Why Religion?: A Personal Story", "Elaine Pagels", "256", "Ecco", "2018" }
-        };
+        public void output() {
+                System.out.printf("1. %-15s: %s\n" + "2. %-15s: %s\n" + "3. %-15s: %s halaman\n"
+                                + "4. %-15s: %s\n" + "5. %-15s: %s\n\n", "Judul Buku", judul, "Penulis", penulis,
+                                "Jumlah Halaman", jumlahHalaman,
+                                "Sinopsis", sinopsis, "Jumlah Kata di Sinopsis", hitungJumlahKataSinopsis());
+        }
 
-        public String[][] Psikologi = {
-                        { "Man's Search for Meaning", "Viktor E. Frankl", "165", " Beacon Press", "1959" },
-                        { "The Power of Habit: Why We Do What We Do in Life and Business", "Charles Duhigg", "400",
-                                        "Random House",
-                                        "2012" },
-                        { "Quiet: The Power of Introverts in a World That Can't Stop Talking", "Susan Cain", "352",
-                                        "Crown Publishing Group", "2012" },
-                        { "Blink: The Power of Thinking Without Thinking", "Malcolm Gladwell", "296",
-                                        "Little, Brown and Company",
-                                        "2005" },
-                        { "Influence: The Psychology of Persuasion", " Robert B. Cialdini", " 336", "HarperCollins",
-                                        "2006" }
-        };
-
-        public String[][] Politik = {
-                        { "The Prince", "Niccolò Machiavelli", "Varied (tergantung edisi)",
-                                        "Various", "1532" },
-                        { "The Road to Serfdom", "Friedrich Hayek", "283", "University of Chicago Press", "1944" },
-                        { "The New Jim Crow: Mass Incarceration in the Age of Colorblindness", "Michelle Alexander",
-                                        "352",
-                                        "The New Press", "2010" },
-                        { "How Democracies Die", "Steven Levitsky, Daniel Ziblatt", "320", "Crown Publishing Group",
-                                        "2018" },
-                        { "On Tyranny: Twenty Lessons from the Twentieth Century", "Timothy Snyder", "128",
-                                        "Tim Duggan Books",
-                                        "2017" }
-        };
-
-        public String[][] Fiksi = {
-                        { "The Kite Runner", " Khaled Hosseini", " 391", "Riverhead Books", "2003" },
-                        { "The Hunger Games", "Suzanne Collins", "374", "Scholastic Press", "2008" },
-                        { "The Girl with the Dragon Tattoo", "Stieg Larsson", "465", " Norstedts Förlag", "2005" },
-                        { "The Road", "Cormac McCarthy", "287", "Alfred A. Knopf", "2006" },
-                        { "The Alchemist", "Paulo Coelho", "197", "HarperCollins", "1988" }
-        };
-
-        public void showtype(String[][] type, String categoryType) {
-                System.out.printf("Kategori %s\n\n", categoryType);
-                for (int i = 0; i < type.length; i++) {
-                        System.out.printf("Buku ke-%d\n"
-                                        + "1. %-15s: %s\n" + "2. %-15s: %s\n" + "3. %-15s: %s halaman\n"
-                                        + "4. %-15s: %s\n" + "5. %-15s: %s\n\n",
-                                        i + 1, "Judul Buku", type[i][0], "Penulis", type[i][1], "Jumlah Halaman",
-                                        type[i][2],
-                                        "Penerbit", type[i][3], "Tahun Terbit", type[i][4]);
+        public void kategori(Perpustakaan[] libraryArray) {
+                int i = 0;
+                for (Perpustakaan library : libraryArray) {
+                        i++;
+                        if (i <= libraryArray.length)
+                                System.out.println("Buku yang ke-" + i);
+                        library.output();
                 }
         }
 }
